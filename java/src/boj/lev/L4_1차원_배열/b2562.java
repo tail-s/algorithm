a6cd80d6fddc9092ccd0cf86e8c0cececb9a9fc6
tail-s <lunar7s@naver.com>
2024-02-09 23:59:47 +0900

    240209

--- java/src/boj/lev/L4_1차원_배열/b2562.java
+++ java/src/boj/lev/L4_1차원_배열/b2562.java
@@ -5,5 +5,18 @@ import java.util.Scanner;
 public class b2562 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int[] arr = new int[9];
+        int max = -1, idx = -1;
+        for (int i=0; i<9; i++) {
+            arr[i] = sc.nextInt();
+            if (max < arr[i]) {
+                max = arr[i];
+                idx = i + 1;
+            }
+        }
+        sc.close();
+        System.out.println(max + "\n" + idx);
+
+
     }
 }
