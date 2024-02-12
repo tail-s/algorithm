93791a155011cb536aa1e55f9746c08deb2b5a0a
tail-s <lunar7s@naver.com>
2024-02-12 09:00:13 +0900

    240212

--- java/src/boj/lev/L6_심화_1/b2444.java
+++ java/src/boj/lev/L6_심화_1/b2444.java
@@ -1,7 +1,25 @@
 package boj.lev.L6_심화_1;
 
+import java.util.Scanner;
+
 public class b2444 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        sc.close();
+
+        StringBuilder sb = new StringBuilder();
+        for (int i=0; i<N; i++) {
+            for (int j=0; j<N-i-1; j++) sb.append(" ");
+            for (int j=0; j<2*i+1; j++) sb.append("*");
+            sb.append("\n");
+        }
+        for (int i=N-1; i>0; i--) {
+            for (int j=0; j<N-i; j++) sb.append(" ");
+            for (int j=0; j<2*i-1; j++) sb.append("*");
+            sb.append("\n");
+        }
+        System.out.println(sb);
 
     }
 }
