416c561070769919702c7007ec9ae5732778eee6
tail-s <lunar7s@naver.com>
2024-02-14 05:19:04 +0900

    240214

--- java/src/boj/lev/L8_일반_수학_1/b2745.java
+++ java/src/boj/lev/L8_일반_수학_1/b2745.java
@@ -1,4 +1,14 @@
 package boj.lev.L8_일반_수학_1;
 
+import java.util.Scanner;
+
 public class b2745 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String N = sc.next();
+        int B = sc.nextInt();
+        sc.close();
+
+
+    }
 }
