46c0477b7e0271ebf389640520c61ee8924c2dd5
tail-s <lunar7s@naver.com>
2024-02-19 17:36:54 +0900

    240219

--- java/src/boj/lev/L8_일반_수학_1/b11005.java
+++ java/src/boj/lev/L8_일반_수학_1/b11005.java
@@ -3,12 +3,19 @@ package boj.lev.L8_일반_수학_1;
 import java.util.Scanner;
 
 public class b11005 {
+    public static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int B = sc.nextInt();
         sc.close();
 
+        while (N != 0) {
+            sb.append(N % B < 10 ? (char) (N % B + '0') : (char) (N % B - 10 + 'A'));
+            N /= B;
+        }
+
+        System.out.println(sb.reverse());
 
     }
 }
