d4be24d4fca353b0de34f0c72c2fd534f5f499e2
tail-s <lunar7s@naver.com>
2024-02-21 07:09:19 +0900

    240221

--- java/src/boj/lev/L8_일반_수학_1/b1193.java
+++ java/src/boj/lev/L8_일반_수학_1/b1193.java
@@ -5,5 +5,19 @@ import java.util.Scanner;
 public class b1193 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt();
+        sc.close();
+
+        int layer = 1, L = -1, R = -1;
+        while (X > layer) {
+            X -= layer;
+            layer++;
+        }
+
+        boolean flag = layer % 2 == 0;
+
+        R = flag ? layer - X + 1 : X;
+        L = flag ? X : layer - X + 1;
+        System.out.println(L + "/" + R);
     }
 }
