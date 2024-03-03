10f08052a5794c38b1b3912fdd6e85c37024778d
tail-s <lunar7s@naver.com>
2024-03-04 05:16:53 +0900

    240304

--- java/src/boj/lev/L12_브루트_포스/b1018.java
+++ java/src/boj/lev/L12_브루트_포스/b1018.java
@@ -9,14 +9,21 @@ public class b1018 {
     public static char[] setA = new char[] {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'};
     public static char[] setB = new char[] {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'};
 
-    public static void check() {
+    public static void check(int r, int c) {
         int cnt = 0;
 
-        for (int i=0; i<N-7; i++)
-            for (int j=0; j<M-7; j++) {
-                                                
-            }
+        for (int i=r; i<r+8; i++)
+            for (int j=c; j<c+8; j++)
+                cnt += i % 2 == 0 ? map[i][j] != setA[j-c] ? 1 : 0 : map[i][j] != setB[j-c] ? 1 : 0;
 
+        ans = Math.min(ans, cnt);
+        cnt = 0;
+
+        for (int i=r; i<r+8; i++)
+            for (int j=c; j<c+8; j++)
+                cnt += i % 2 == 0 ? map[i][j] != setB[j-c] ? 1 : 0 : map[i][j] != setA[j-c] ? 1 : 0;
+
+        ans = Math.min(ans, cnt);
     }
 
     public static void main(String[] args) {
@@ -28,8 +35,11 @@ public class b1018 {
         for (int i=0; i<N; i++) map[i] = sc.next().toCharArray();
         sc.close();
 
+        for (int i=0; i<N-7; i++)
+            for (int j=0; j<M-7; j++)
+                check(i, j);
 
-
+        System.out.println(ans);
 
     }
 }
