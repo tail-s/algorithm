f096d639824c315f7e52498df19e8c6a9b4379fa
tail-s <lunar7s@naver.com>
2024-03-10 22:49:45 +0900

    240310

--- java/src/boj/chap/C13_Backtracking/b14888.java
+++ java/src/boj/chap/C13_Backtracking/b14888.java
@@ -5,15 +5,36 @@ import java.util.Scanner;
 public class b14888 {
     public static int N, max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
     public static int[] arr, ops = new int[4];
+    public static int cal(int value, int ops, int target) {
+        int result = value;
+        switch (ops) {
+            case 0:
+                result += target;
+                break;
+            case 1:
+                result -= target;
+                break;
+            case 2:
+                result *= target;
+                break;
+            case 3:
+                result /= target;
+        }
+        return result;
+    }
     public static void dfs(int depth, int value) {
-        if (depth == N) {
+        if (depth == N-1) {
             max = Math.max(max, value);
             min = Math.min(min, value);
             return;
         }
 
         for (int i=0; i<4; i++) {
-
+            if (ops[i] > 0) {
+                ops[i]--;
+                dfs(depth+1, cal(value, i, arr[depth+1]));
+                ops[i]++;
+            }
         }
     }
 
@@ -26,7 +47,8 @@ public class b14888 {
         for (int i=0; i<4; i++) ops[i] = sc.nextInt();
         sc.close();
 
-        dfs(0);
+        dfs(0, arr[0]);
+        System.out.println(max + "\n" + min);
 
     }
 }
