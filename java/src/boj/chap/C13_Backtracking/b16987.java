882e26fa685fc25aedffb52230517866dddcb1df
tail-s <lunar7s@naver.com>
2024-03-12 01:01:21 +0900

    240311

--- java/src/boj/chap/C13_Backtracking/b16987.java
+++ java/src/boj/chap/C13_Backtracking/b16987.java
@@ -1,7 +1,9 @@
 package boj.chap.C13_Backtracking;
 
+import java.util.Scanner;
+
 public class b16987 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
     }
 }
