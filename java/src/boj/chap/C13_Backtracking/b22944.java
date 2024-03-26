ab931d31dbb75781a4c7b0444c16f3f4f6d1c21a
tail-s <lunar7s@naver.com>
2024-03-27 04:46:19 +0900

    240327

--- java/src/boj/chap/C13_Backtracking/b22944.java
+++ java/src/boj/chap/C13_Backtracking/b22944.java
@@ -1,11 +1,41 @@
 package boj.chap.C13_Backtracking;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class b22944 {
-    public static int N, H, D;
+    public static int N, H, D, K, s_r, s_c, e_r, e_c, ans = Integer.MAX_VALUE;
     public static char[][] map;
-    public static List<int[]> 
+    public static boolean[] visited;
+    public static List<int[]> umbrella = new ArrayList<>();
+    public static void dfs(int depth, int step, int r, int c, int hp, int u) {
+        int end = Math.abs(e_r - r) + Math.abs(e_c - c);
+        if (step + end >= ans) return;
+        if (end <= hp + u) {
+            ans = Math.min(ans, step + Math.abs(e_r - r) + Math.abs(e_c - c));
+            return;
+        }
+        if (depth == K+1) return;
+
+
+        for (int i=0; i<K; i++) {
+            if (visited[i]) continue;
+            int[] target = umbrella.get(i);
+            int dist = Math.abs(target[0] - r) + Math.abs(target[1] - c);
+            if (dist > hp + u) continue;
+            visited[i] = true;
+            int thp = 0;
+            if (u >= dist) {
+                thp = hp;
+            } else {
+                thp = hp + u - dist;
+            }
+            dfs(depth+1, step + Math.abs(target[0] - r) + Math.abs(target[1] - c), target[0], target[1], thp, D);
+            visited[i] = false;
+
+        }
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
@@ -17,11 +47,26 @@ public class b22944 {
             String str = sc.next();
             for (int j=0; j<N; j++) {
                 map[i][j] = str.charAt(j);
-                if (map[i][j] == 'E')
+                if (map[i][j] == 'S') {
+                    s_r = i;
+                    s_c = j;
+                } else if (map[i][j] == 'E') {
+                    e_r = i;
+                    e_c = j;
+                } else if (map[i][j] == 'U') {
+                    umbrella.add(new int[]{i, j});
+                }
             }
         }
 
         sc.close();
+
+        K = umbrella.size();
+        visited = new boolean[K];
+
+        dfs(0, 0, s_r, s_c, H, 0);
+        ans = ans == Integer.MAX_VALUE ? -1 : ans;
+        System.out.println(ans);
 
 
 
