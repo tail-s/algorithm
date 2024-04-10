6831ca2d6088df031d6290c26054c1eeb98de0b6
tail-s <lunar7s@naver.com>
2024-04-11 04:03:18 +0900

    240411

--- java/src/boj/chap/C1_Data_Structure/b2346_solving.java
+++ java/src/boj/chap/C1_Data_Structure/b2346_solving.java
@@ -1,7 +1,35 @@
 package boj.chap.C1_Data_Structure;
 
-public class b2346_solving {
-    public static void main(String[] args) {
+import java.io.*;
+import java.util.*;
 
+public class b2346_solving {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        Deque<Map.Entry<Integer, Integer>> d = new ArrayDeque<>();  //LinkedList 메모리초과
+        Map.Entry<Integer, Integer> tmp;
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for(int i=1; i<=n; i++)
+            d.offer(new AbstractMap.SimpleEntry(i, Integer.parseInt(st.nextToken())));
+
+        while(!d.isEmpty()) {
+            tmp = d.poll();
+            sb.append(tmp.getKey() + " ");
+            if(!d.isEmpty() && tmp.getValue() > 0)	//풍선 속 번호가 양수
+                for(int j=0; j<tmp.getValue()-1; j++)
+                    d.offer(d.poll());
+            else if(!d.isEmpty() && tmp.getValue() < 0)
+                for(int j=0; j<Math.abs(tmp.getValue()); j++)
+                    d.offerFirst(d.pollLast());
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
