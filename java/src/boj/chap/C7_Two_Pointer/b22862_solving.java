6af994ec522b5de2d699f935c921c2231ed8786f
tail-s <lunar7s@naver.com>
2024-06-08 00:23:44 +0900

    ,

---
+++ java/src/boj/chap/C7_Two_Pointer/b22862_solving.java
@@ -0,0 +1,16 @@
+package boj.chap.C7_Two_Pointer;
+
+import java.util.Scanner;
+
+public class b22862_solving {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) { arr[i] = sc.nextInt(); }
+        sc.close();
+
+        
+    }
+}
