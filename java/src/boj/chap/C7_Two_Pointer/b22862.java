0428925d6ba01c9e178f992ba1938fc685a0356e
tail-s <lunar7s@naver.com>
2024-06-10 04:24:28 +0900

    .

--- java/src/boj/chap/C7_Two_Pointer/b22862.java
+++ java/src/boj/chap/C7_Two_Pointer/b22862.java
@@ -7,9 +7,26 @@ public class b22862 {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int K = sc.nextInt();
-        int[] arr = new int[N];
-        for (int i = 0; i < N; i++) { arr[i] = sc.nextInt(); }
+        int[] S = new int[N];
+        for (int i = 0; i < N; i++) { S[i] = sc.nextInt(); }
         sc.close();
+
+        int s = 0, e = 0, del = 0, cnt = 0, ans = 0;
+        while (e < N) {
+            if (S[e] % 2 != 0) {
+                if (del < K) {
+                    del++;
+                    e++;
+                } else {
+                    while (del == K) {
+                        if (S[s] % 2 != 0) del--;
+                        s++;
+                    }
+                }
+            } else e++;
+            ans = Math.max(ans, e - s - del);
+        }
+        System.out.println(ans);
 
         
     }
