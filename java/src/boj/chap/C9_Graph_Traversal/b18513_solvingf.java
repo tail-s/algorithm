62eaa72bbcd734abb653a0af63b2345b983b1e73
tail-s <lunar7s@naver.com>
2024-08-02 23:47:39 +0900

    .

--- java/src/boj/chap/C9_Graph_Traversal/b18513_solvingf.java
+++ java/src/boj/chap/C9_Graph_Traversal/b18513_solvingf.java
@@ -3,14 +3,41 @@ package boj.chap.C9_Graph_Traversal;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class b18513_solvingf {
     static int N, K;
-    static boolean[] visited = new boolean[200200001];
-    static List<Integer> list = new ArrayList<Integer>();
+    static long ans = 0;
+    static boolean[] visited = new boolean[200200002];
+    static Queue<Integer> q = new ArrayDeque<>();
+
+    static void bfs() {
+        int len = 1;
+
+        build:
+        while (!q.isEmpty()) {
+            int size = q.size();
+            while (size-- > 0) {
+                int pos = q.poll();
+
+                if (!visited[pos - 1]) {
+                    q.offer(pos - 1);
+                    visited[pos - 1] = true;
+                    ans += len;
+                    if (--K == 0) break build;
+                }
+
+                if (!visited[pos + 1]) {
+                    q.offer(pos + 1);
+                    visited[pos + 1] = true;
+                    ans += len;
+                    if (--K == 0) break build;
+                }
+            }
+            len++;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] input = br.readLine().split(" ");
@@ -19,9 +46,14 @@ public class b18513_solvingf {
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         while (st.hasMoreTokens()) {
-            int cur = Integer.parseInt(st.nextToken());
-            list.add
+            int cur = Integer.parseInt(st.nextToken()) + 100100000;
+            q.offer(cur);
+            visited[cur] = true;
         }
+
+        bfs();
+
+        System.out.println(ans);
 
 
 
