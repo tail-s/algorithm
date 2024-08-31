95c59bd2955ffd04ef901ab050ff5a2a9e4193f6
tail-s <lunar7s@naver.com>
2024-09-01 00:46:59 +0900

    .

--- java/src/boj/chap/C10_Brute_Force/b14391.java
+++ java/src/boj/chap/C10_Brute_Force/b14391.java
@@ -3,10 +3,70 @@ package boj.chap.C10_Brute_Force;
 import java.util.Scanner;
 
 public class b14391 {
-    static int N, M;
+    static int N, M, ANS = Integer.MIN_VALUE;
+    static char[][] map;
+    static boolean[][] visited;
+
+    static void calculate() {
+        int result = 0;
+
+        for (int i = 0; i < N; i++) {
+            int sum = 0;
+            for (int j = 0; j < M; j++) {
+                if (visited[i][j]) {
+                    sum = sum * 10 + (map[i][j] - '0');
+                } else {
+                    result += sum;
+                    sum = 0;
+                }
+            }
+            result += sum;
+        }
+
+        for (int j = 0; j < M; j++) {
+            int sum = 0;
+            for (int i = 0; i < N; i++) {
+                if (!visited[i][j]) {
+                    sum = sum * 10 + (map[i][j] - '0');
+                } else {
+                    result += sum;
+                    sum = 0;
+                }
+            }
+            result += sum;
+        }
+
+        ANS = Math.max(ANS, result);
+    }
+
+    static void solve(int cur) {
+        if (cur == N * M) {
+            calculate();
+            return;
+        }
+
+        int r = cur / M;
+        int c = cur % M;
+
+        visited[r][c] = true;
+        solve(cur + 1);
+        visited[r][c] = false;
+        solve(cur + 1);
+
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        map = new char[N][M];
+        visited = new boolean[N][M];
+        for (int i = 0; i < N; i++) map[i] = sc.next().toCharArray();
+        sc.close();
 
+        solve(0);
+
+        System.out.println(ANS);
 
     }
 }
