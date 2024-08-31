7e2bf7eb3654b99ad171a8cb48ee48073f7f71f5
tail-s <lunar7s@naver.com>
2024-08-31 19:50:33 +0900

    .

---
+++ java/src/boj/chap/C10_Brute_Force/b14391.java
@@ -0,0 +1,12 @@
+package boj.chap.C10_Brute_Force;
+
+import java.util.Scanner;
+
+public class b14391 {
+    static int N, M;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+    }
+}
