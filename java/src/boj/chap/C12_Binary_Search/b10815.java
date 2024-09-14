e370e48b3b30028153c6b4b07a88dd733d8d2e8f
tail-s <lunar7s@naver.com>
2024-09-14 21:17:12 +0900

    .

---
+++ java/src/boj/chap/C12_Binary_Search/b10815.java
@@ -0,0 +1,49 @@
+package boj.chap.C12_Binary_Search;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class b10815 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+//        int N = sc.nextInt();
+//        HashSet<Integer> target = new HashSet<Integer>();
+//        while (N-- > 0) target.add(sc.nextInt());
+//        int M = sc.nextInt();
+//        while (M-- > 0) sb.append(target.contains(sc.nextInt()) ? "1 " : "0 ");
+//        sb.setLength(sb.length()-1);
+//        sc.close();
+//        System.out.println(sb.toString());
+
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) arr[i] = sc.nextInt();
+        Arrays.sort(arr);
+        int M = sc.nextInt();
+        while (M-- > 0) {
+            int target = sc.nextInt();
+            int s = 0;
+            int e = N - 1;
+            while (s <= e) {
+                int mid = (s + e) / 2;
+                int val = arr[mid];
+                if (val == target) {
+                    sb.append("1 ");
+                    break;
+                }
+
+                if (val > target) e = mid - 1;
+                else s = mid + 1;
+
+                if (s > e) {
+                    sb.append("0 ");
+                    break;
+                }
+            }
+        }
+        System.out.println(sb.toString());
+    }
+}
---
+++ java/src/boj/chap/C12_Binary_Search/b1789.java
@@ -0,0 +1,56 @@
+package boj.chap.C12_Binary_Search;
+
+import java.util.Scanner;
+
+public class b1789 {
+
+    static long S, ans;
+    static long _sum(long N) { return (1 + N) * N / 2; }
+
+    static void binarySearch(long s, long e) {
+
+//        if (s > e) return;
+//        long mid = (s + e) / 2;
+//        long sum = _sum(mid);
+//
+//        if (sum > S) binarySearch(s, mid - 1);
+//        else if (sum < S) {
+//            ans = Math.max(ans, mid);
+//            binarySearch(mid + 1, e);
+//        }
+//        else ans = mid;
+
+
+        while (s <= e) {
+            long mid = (s + e) / 2;
+            long sum = _sum(mid);
+
+            if (sum <= S) {
+                ans = mid;
+                s = mid + 1;
+            } else e = mid - 1;
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        S = sc.nextLong();
+        sc.close();
+        ans = 0L;
+
+//        for (int i = 1; sum <= S; i++) {
+//            sum += i;
+//            ans++;
+//        }
+//        ans--;
+//        System.out.println(ans);
+
+        binarySearch(1, S);
+        System.out.println(ans);
+
+
+
+    }
+}
